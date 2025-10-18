11423aa6f4ed2e37c35ec618ea9895b1e8d7c584
DaSilvaLuis13 <gomezsilvaluisfelipe@gmail.com>
2025-10-17 19:05:17 -0600

    Nadita

--- src/pages/Devoluciones.jsx
+++ src/pages/Devoluciones.jsx
@@ -1,21 +1,3 @@
-//Aquí van los import que necesites incorporar elementos de la carpeta components
-
- rama-salarioCliente
-// Aquí van los import que necesites
-
-// Si tienes componentes personalizados:
-// import SearchBar from "../components/SearchBar";
-// import ProductTable from "../components/ProductTable";
-// import MetodoDevolucion from "../components/MetodoDevolucion";
-
-function Devoluciones() {
-  // Estados principales
-
-  return (
-    <div className="container mt-4">
-      <h1>Devoluciones</h1>
-    </div>
-  );
 
 /* 
 Formulario para registrar devoluciones
